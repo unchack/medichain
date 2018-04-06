eccd869cc7112b93914d9043634932c9a0c1fbb3
Emil Nikolov <emil.e.nikolov@gmail.com>
2018-04-06 17:27:05 +0200

    added a script to copy ssh keys to pis

---
+++ pis.sh
@@ -0,0 +1,5 @@
+sshpass -p welkom34 ssh-copy-id -i pi@pi-commander-1.local
+sshpass -p welkom34 ssh-copy-id -i pi@pi-1.local
+sshpass -p welkom34 ssh-copy-id -i pi@pi-2.local
+sshpass -p welkom34 ssh-copy-id -i pi@pi-3.local
+sshpass -p welkom34 ssh-copy-id -i pi@pi-4.local
